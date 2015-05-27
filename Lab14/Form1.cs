7488af501cede4d91ec0c988c4bc5b522d3ccbdb
btomashvili <bekatomashvili@gmail.com>
2015-05-27 17:54:34 +0400

    Lab14 Linq Samples

---
+++ Lab14/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UsersPrject
+{
+
+    /// <summary>
+    /// This project is for test purposes only for students ;
+    /// TODO 1) Update user and save methods do it in one method ;
+    /// TODO 2) Use Try Catch Statement for methods ;
+    /// TODO 3) Good validation for user properties ;
+    /// TODO 4) Confirm Message Box for delete operation;
+    /// </summary>
+
+    public partial class Form1 : Form
+    {
+      
+        int userId = 0;
+       
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+           
+            
+            if (userId > 0)
+            {
+                using (TEMPEntities db = new TEMPEntities())
+                {
+                    User u = db.Users.First(x => x.Id == userId);
+                    u.Name = txtUser.Text;
+                    u.Surname = txtSurname.Text;
+                    db.SaveChanges();
+                }
+            }
+            else
+            {
+                SaveUser();
+            }
+
+
+            LoadUsers();
+        }
+
+        private void SaveUser()
+        {
+
+            using (TEMPEntities db = new TEMPEntities())
+            {
+                User user = new User();
+                user.Name = txtUser.Text;
+                user.Surname = txtSurname.Text;
+
+                int position = Convert.ToInt32(comboPositions.SelectedValue);
+                user.PositionId = position;
+
+                db.Users.Add(user);
+                db.SaveChanges();
+             
+
+                txtSurname.Text = string.Empty;
+                txtUser.Text = string.Empty;
+
+            }
+
+        
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+            LoadUsers();
+
+          
+
+        }
+
+        private void LoadUsers()
+        {
+            using (TEMPEntities db = new TEMPEntities())
+            {
+               // var listUsers = db.Users.ToList();
+               // var postions = db.Positions.ToList();
+               
+                var userPositions  =
+                    (from u in db.Users
+                     join p in db.Positions on u.PositionId equals p.ID into ps
+                     from p in ps.DefaultIfEmpty()
+                    select new {
+                                 UserId = u.Id,   
+                                 Name = u.Name, 
+                                 Surname = u.Surname,
+                                 UserPosition = (p==null) ? string.Empty: p.Name
+                         }
+                        ).ToList();
+
+              
+                BindingSource bs = new BindingSource();
+                bs.DataSource = userPositions;
+                dataGridView1.DataSource = bs;
+
+                
+
+
+            }
+        }
+
+        private void dataGridView1_CellMouseDoubleClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+           
+
+
+        }
+
+        private void deleteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var user = userBindingSource.Current as User;
+
+            if (user == null) return;
+
+            using (TEMPEntities db = new TEMPEntities())
+            {
+                var u = db.Users.First(x => x.Id == user.Id);
+                db.Users.Remove(u);
+                db.SaveChanges();
+                LoadUsers();
+                
+            }
+
+        }
+
+        private void editToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var user = userBindingSource.Current as User;
+
+            if (user == null) return;
+
+            userId = user.Id;
+            txtUser.Text = user.Name;
+            txtSurname.Text = user.Surname;
+
+        }
+    }
+}
---
+++ Lab14/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("UsersPrject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UsersPrject")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("214cd64c-16bd-4d00-b90a-c3de0d13eca7")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
